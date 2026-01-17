d68acc74e318840a256970c7ba4beedcd74fe327
Deekshith Mamidi <mamidideekshith19th@gmail.com>
2026-01-17 16:53:18 +0530

    Delete BazaarCast/src directory

--- BazaarCast/src/train.py
+++
@@ -1,9 +0,0 @@
-from sklearn.metrics import mean_absolute_error
-
-def train_model(model, train_df):
-    model.fit(train_df[["t"]], train_df["Sales"])
-    return model
-
-def evaluate_model(model, test_df):
-    preds = model.predict(test_df[["t"]])
-    return mean_absolute_error(test_df["Sales"], preds)
